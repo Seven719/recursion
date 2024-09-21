539d8aec9484c420ae1f99c691f7ed80cc7c8c96
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-09-21 09:25:55 +0100

    completed fibonacci sequence using a recursive function

--- fibonacci.js
+++ fibonacci.js
@@ -9,3 +9,14 @@ const fibs = (n) => {
 };
 
 console.log(fibs(8));
+
+const fibsRecursive = (n, arr = [0, 1]) => {
+  if (n == 1) return [0];
+  if (n == 2) return arr;
+  return fibsRecursive(n - 1, [
+    ...arr,
+    arr[arr.length - 1] + arr[arr.length - 2],
+  ]);
+};
+
+console.log(fibsRecursive(8));
